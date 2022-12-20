63fdcc1bf5474c63edf320623fac58e1f1bfd56d
Lucas Silva <lucas_55i@outlook.com>
2022-12-20 20:47:41 -0300

    Criando Conexão com o Banco de Dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [PostsModule],
+  imports: [MongooseModule.forRoot(
+    'mongodb://root:example@localhost:27017',
+  )
+    ,PostsModule],
   controllers: [],
   providers: [],
 })
