9d5aff1e68bdd712ec93e0ab89724cb928935e01
Lucas Silva <lucas_55i@outlook.com>
2022-12-20 21:14:44 -0300

    Implementação do metodo de criação de post

--- src/posts/controller/posts.controller.ts
+++ src/posts/controller/posts.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { CriarPostDto } from '../dtos/criar-post.dto';
+import { Posts } from '../interface/post.interface';
 import { PostsService } from '../service/posts.service';
 
 @Controller('api/v1/posts')
@@ -10,6 +12,12 @@ export class PostsController {
     @Get()
     async consultarPosts(){
         return this.postService.consultarPosts()
+    }
+
+    @Post()
+    @UsePipes(ValidationPipe)
+    async criarPost(@Body() criarPostDto: CriarPostDto): Promise<Posts>{
+        return await this.postService.criarPost(criarPostDto)
     }
 
 }
--- src/posts/service/posts.service.ts
+++ src/posts/service/posts.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { CriarPostDto } from '../dtos/criar-post.dto';
 import { Posts } from '../interface/post.interface';
 
 @Injectable()
@@ -10,8 +11,13 @@ export class PostsService {
         @InjectModel('Posts') private readonly postsModel: Model<Posts>
     ){}
 
-    async consultarPosts(){
+    async consultarPosts(): Promise<Array<Posts>>{
         return await this.postsModel.find().exec()
+    }
+    
+    async criarPost(criarPostDto: CriarPostDto):Promise<Posts> {
+        const postCriado = new this.postsModel(criarPostDto)
+        return await postCriado.save()
     }
 
 }
