34c6b877e1b2ec539599f5880048c58feefdfccc
Lucas Silva <lucas_55i@outlook.com>
2022-12-21 10:01:28 -0300

    Atualização do formato para criar um Post

--- src/posts/dtos/criar-post.dto.ts
+++ src/posts/dtos/criar-post.dto.ts
@@ -1,9 +1,10 @@
 import { IsNotEmpty, IsString } from "class-validator"
+import { User } from "src/users/interface/user.interface"
 
-export class CriarPostDto{
+export class CriarPostDto {
     @IsString()
     @IsNotEmpty()
-    author: string
+    author: User
 
     @IsString()
     @IsNotEmpty()
--- src/posts/service/posts.service.ts
+++ src/posts/service/posts.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CriarPostDto } from '../dtos/criar-post.dto';
@@ -6,18 +6,24 @@ import { Posts } from '../interface/post.interface';
 
 @Injectable()
 export class PostsService {
-    
+
     constructor(
         @InjectModel('Posts') private readonly postsModel: Model<Posts>
-    ){}
+    ) { }
 
-    async consultarPosts(): Promise<Array<Posts>>{
+    async consultarPosts(): Promise<Array<Posts>> {
         return await this.postsModel.find().exec()
     }
-    
-    async criarPost(criarPostDto: CriarPostDto):Promise<Posts> {
-        const postCriado = new this.postsModel(criarPostDto)
-        return await postCriado.save()
-    }
 
+    async criarPost(criarPostDto: CriarPostDto): Promise<Posts> {
+        const { author } = criarPostDto;
+
+        const usuarioEncontrado = await this.postsModel.findOne({ author }).exec();
+
+        if (!usuarioEncontrado) {
+            throw new BadRequestException(`Autor com nome: ${author} não encontrado`)
+        }
+        const jogadorCriado = new this.postsModel(criarPostDto);
+        return await jogadorCriado.save();
+    }
 }
