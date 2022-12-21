ed8e38e602a98e10154a010be959908f82bcfa81
Lucas Silva <lucas_55i@outlook.com>
2022-12-21 11:15:44 -0300

    Implementação de ApiProperty

--- src/posts/dtos/criar-post.dto.ts
+++ src/posts/dtos/criar-post.dto.ts
@@ -1,11 +1,20 @@
+import { ApiProperty } from "@nestjs/swagger"
 import { IsNotEmpty, IsString } from "class-validator"
 import { User } from "src/users/interface/user.interface"
 
 export class CriarPostDto {
+    @ApiProperty({
+        description: "Autor do texto, herda do USER",
+        example: "Lucas",
+    })
     @IsString()
     @IsNotEmpty()
     author: User
 
+    @ApiProperty({
+        description: "Conteúdo do testo",
+        example: "Conteúdo de Teste",
+    })
     @IsString()
     @IsNotEmpty()
     conteudo: string
--- src/users/dtos/criar-user.dto.ts
+++ src/users/dtos/criar-user.dto.ts
@@ -1,6 +1,11 @@
+import { ApiProperty } from "@nestjs/swagger"
 import { IsNotEmpty, IsString } from "class-validator"
 
-export class CriarUserDto{
+export class CriarUserDto {
+    @ApiProperty({
+        description: "Nome do usuário",
+        example: "Lucas",
+    })
     @IsString()
     @IsNotEmpty()
     nome: string
